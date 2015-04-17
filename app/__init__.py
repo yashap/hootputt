f1c073ec67fa6e5fecacdd67b342b3351444662e
yasha-podeswa-hs <yasha.podeswa@hootsuite.com>
2015-04-16 18:19:32 -0700

    Bunch of gross code

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ import logging
 app = Flask(__name__)
 
 # Add config to app
-app.config.from_object(__name__)
+app.config.from_object('config')
+app.secret_key = 'imsosecretyeah1'
 
 # Import our views
-from app.views import *
+from app.views import index
--- app/views/index.py
+++ app/views/index.py
@@ -1,8 +1,21 @@
 from app import app
 import flask
+from flask.ext.wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return flask.render_template('index.html', title='Home')
+    form = HootputtForm()
+
+    if form.validate_on_submit():
+        print 'It worked! name: %s, strokes: %s' % (form.name.data, form.strokes.data)
+
+    return flask.render_template('index.html',
+      title='Hootputt',
+      form=form)
+
+class HootputtForm(Form):
+    name = StringField('name', validators=[DataRequired()])
+    strokes = StringField('strokes', validators=[DataRequired()])
